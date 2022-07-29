add4d03482a83e22140e3c7d0a06a6b61dd744a4
debbyra <deborahrahj20@gmail.com>
2022-07-29 01:11:51 -0700

    pythonprogrammingtest1

---
+++ test1.py
@@ -0,0 +1,54 @@
+# #no.1(a)
+classroom = ("name","age","school")
+dict2 = (("alex",15,"codehighschool"),("junior",20,"kitanteschool"),("chris",17,"namanvehigh"),("adam",19,"nativehigh"))
+all_students=(classroom+dict2)
+for all_students in classroom: (classroom+dict2 in zip(all_students))
+
+if classroom==dict2:
+     print(all_students)
+    
+# #(b)
+
+subjects=("Math","English","Chemistry","Biology","ICT")
+dict3=(dict2+subjects)
+print(dict3)
+
+# #(c)
+
+
+
+#no.2(a)
+from audioop import reverse
+
+
+def grade():
+     grade=("A","B+","C","D","E","F")
+     print("grade")
+
+    
+     user_input=("Enter your score:" )
+     return user_input
+
+
+subjects=("Maths","English","Chemistry","Biology","ICT")
+
+
+#no.3(a)
+from audioop import reverse
+
+
+list1=[10,20,30,40]
+list2=[100,200,300,400]
+
+for list1 in list2:
+    reverse 
+    
+    print(list)
+
+
+
+
+
+
+
+
